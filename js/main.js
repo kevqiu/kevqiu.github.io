acd268bab511c7494f4d69ade841aecbeee15df4
Kevin Qiu <kevinqiu96@gmail.com>
2017-02-20 20:44:29 -0500

    finished main animations

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    var canvas1 = $("#upper-line")[0];
+    var c1 = canvas1.getContext("2d");
+
+    var canvas2 = $("#lower-line")[0];
+    var c2 = canvas2.getContext("2d");
+
+    var startX1 = 0;
+    var startX2 = canvas2.width;
+    var y = 10;
+    var endX1 = canvas1.width;
+    var endX2 = 0;
+    var amount = 0;
+
+    setInterval(function() {
+        amount += 0.01; // change to alter duration
+        if (amount > 1) amount = 1;
+        c1.clearRect(0, 0, canvas1.width, canvas1.height);
+        c1.strokeStyle = "white";
+        c1.moveTo(startX1, y);
+        // lerp : a  + (b - a) * f
+        c1.lineTo(startX1 + (endX1 - startX1) * amount, y);
+        c1.stroke();
+
+        c2.clearRect(0, 0, canvas2.width, canvas2.height);
+        c2.strokeStyle = "white";
+        c2.moveTo(startX2, y);
+        // lerp : a  + (b - a) * f
+        c2.lineTo(startX2 + (endX2 - startX2) * amount, y);
+        c2.stroke();
+    }, 13);
+}
